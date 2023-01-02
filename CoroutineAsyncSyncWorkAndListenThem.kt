8681d29891259af27109f3f2758b3dbdb4d97b91
Rufat Nasirov <53703480+r4m-rufat@users.noreply.github.com>
2023-01-02 17:11:27 +0400

    Update CoroutineAsyncSyncWorkAndListenThem.kt

--- CoroutineAsyncSyncWorkAndListenThem.kt
+++ CoroutineAsyncSyncWorkAndListenThem.kt
@@ -1,23 +1,23 @@
-package com.example.abbtasks
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.route4me.notesapp.R
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
-class Solution : AppCompatActivity() {
-    private val list = mutableListOf<String>()
+class MainActivity : AppCompatActivity() {
+
     private val state = MutableStateFlow("empty")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         observeStateUpdates()
-        
+
     }
 
     private fun observeStateUpdates() {
@@ -28,8 +28,8 @@ class Solution : AppCompatActivity() {
             // observe state updates
             state.collect {
                 // subscribe to flow updates and print state.value to logcat.
-                if (it!="empty")
-                  Log.d("Tag", $it)
+                if (it != "empty")
+                    Log.d("Tag", it)
 
             }
         }
@@ -39,18 +39,16 @@ class Solution : AppCompatActivity() {
         println("runSync method.")
         //  launch 1000 coroutines. Invoke doWork(index/number of coroutine) one after another. Example 1, 2, 3, 4, 5, etc.
 
-            CoroutineScope(IO).launch {
+        lifecycleScope.launch {
 
-                // launch 1000 coroutines with one coroutine and this is associated to synch
-                repeat(1000) {
-                  launch{
-                        list.add(it.toString())
-                    }
-                    delay(300)
-                    doWork(list[it])
+            // launch 1000 coroutines with one coroutine and this is associated to synch
+            repeat(1000) {
+                withContext(IO) {
+                    doWork("$it")
                 }
-
             }
+
+        }
 
 
     }
@@ -59,29 +57,20 @@ class Solution : AppCompatActivity() {
         println("runAsync method.")
 
         /**
-        * launch 1000 coroutines with the same dispatcher "IO" and 
-        * then call the function doWork with "index of coroutine parameter" in async way. Example 1, 2, 5, 3, 4, 8, etc.
-        */
+         * launch 1000 coroutines with the same dispatcher "IO" and
+         * then call the function doWork with "index of coroutine parameter" in async way. Example 1, 2, 5, 3, 4, 8, etc.
+         */
         lifecycleScope.launch {
 
-            async(IO) {
+            repeat(1000) {
 
-                repeat(1000) {
-                   async(IO){
-                        delay(500)
-                        list.add(it.toString())
-                    }
+                CoroutineScope(IO).launch {
 
-                }
-
-                repeat(1000) {
-                    delay(1000)
-                    doWork(list[it])
+                    doWork("$it")
 
                 }
 
             }
-
         }
     }
 
